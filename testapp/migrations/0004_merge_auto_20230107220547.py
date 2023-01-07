da41e1ee878a9ad6b544484d00705742d0ef1fde
ulasozguler <ulasozguler@users.noreply.github.com>
2023-01-07 22:05:48 +0000

    Automatic Django migration merge

---
+++ testapp/migrations/0004_merge_auto_20230107220547.py
@@ -0,0 +1,15 @@
+# Auto-generated merge migration
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('testapp', '0001_initial'),
+        ('testapp', '0002_choice_new_votes'),
+        ('testapp', '0002_later_mig'),
+        ('testapp', '0002_question_new_field'),
+        ('testapp', '0003_merge_auto_20230107214229'),
+    ]
+
