aa9c2873d10204f2806ef3ee68750c934cf8cd7f
ulasozguler <ulasozguler@users.noreply.github.com>
2023-01-08 12:57:37 +0000

    Automatic Django migration merge

---
+++ someapp/migrations/0005_merge_auto_20230108.py
@@ -0,0 +1,12 @@
+# Auto-generated merge migration
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('someapp', '0003_after_merge'),
+        ('someapp', '0004_merge_auto_20230108'),
+    ]
+
