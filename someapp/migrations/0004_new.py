917fef666309e7786572f4aa73c325e7419d8b5e
Ulaş Özgüler <ulas@medsien.com>
2023-01-08 13:45:52 +0300

    test mig

---
+++ someapp/migrations/0004_new.py
@@ -0,0 +1,10 @@
+# Auto-generated merge migration
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("someapp", "0003_merge_auto_20230107214229 copy"),
+    ]
