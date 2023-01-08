19ef47fea636f2293dab28f23a86e34157115193
Ulaş Özgüler <ulas@medsien.com>
2023-01-08 15:56:32 +0300

    test

--- someapp/migrations/0002_question_new_field copy.py
+++ someapp/migrations/0002_question_new_field copy.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("someapp", "0002_question_new_field"),
+        ("someapp", "0001_initial"),
     ]
 
     operations = [
--- someapp/migrations/0004_merge_auto_20230108.py
+++ someapp/migrations/0004_merge_auto_20230108.py
@@ -6,5 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("someapp", "0003_merge_auto_20230107214229 copy"),
+        ('someapp', '0002_choice_new_votes'),
+        ('someapp', '0002_later_mig'),
+        ('someapp', '0003_before_merge'),
     ]
+
